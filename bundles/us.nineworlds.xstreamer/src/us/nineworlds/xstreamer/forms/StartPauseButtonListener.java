5979b0562ffa2d7fea06fb3c6c926e29d12e521a
David Carver <d_a_carver@yahoo.com>
2017-01-10 21:40:10 -0500

    Issue #29 rework the CountDown Timer to use a more reliable thread taskZ

--- bundles/us.nineworlds.xstreamer/src/us/nineworlds/xstreamer/forms/StartPauseButtonListener.java
+++ bundles/us.nineworlds.xstreamer/src/us/nineworlds/xstreamer/forms/StartPauseButtonListener.java
@@ -1,9 +1,20 @@
 package us.nineworlds.xstreamer.forms;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
 import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.joda.time.Period;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
 
 import us.nineworlds.xstreamer.Activator;
 import us.nineworlds.xstreamer.jobs.CountDownJob;
@@ -11,13 +22,13 @@ import us.nineworlds.xstreamer.jobs.CountDownJob;
 public class StartPauseButtonListener implements SelectionListener {
 
 	Button startButton;
-	
-	CountDownJob timerJob;
-	
+
+	Timer countDownTimer;
+
 	public StartPauseButtonListener(Button startButton) {
 		this.startButton = startButton;
 	}
-	
+
 	@Override
 	public void widgetSelected(SelectionEvent e) {
 		String buttonText = startButton.getText();
@@ -27,36 +38,74 @@ public class StartPauseButtonListener implements SelectionListener {
 			startTimer();
 		} else if ("Pause".equals(buttonText)) {
 			startButton.setText("Resume");
-			timerJob.stop();
+			countDownTimer.cancel();
 		} else if ("Resume".equals(buttonText)) {
 			startButton.setText("Pause");
 			startTimer();
-		}		
+		}
 	}
 
 	private void startTimer() {
-		timerJob = new CountDownJob("timer");
-		timerJob.schedule(1000);
+		countDownTimer = new Timer();
+		countDownTimer.schedule(new CountDownTimerTask(), 0, 1000);
 	}
 
 	@Override
 	public void widgetDefaultSelected(SelectionEvent e) {
+
+	}
+
+	private void setInitialTimer() {
+		String inthours = CountDownTimerPage.hourText.getText();
+		String intMins = CountDownTimerPage.minuteText.getText();
+		String intSeconds = CountDownTimerPage.secondsText.getText();
+
+		DateTime startTime = DateTime.now();
+
+		DateTime endTime = startTime.plusHours(Integer.parseInt(inthours)).plusMinutes(Integer.parseInt(intMins))
+				.plus(Integer.parseInt(intSeconds));
+
+		CountDownTimerPage.hourCountDownLabel.setText(CountDownTimerPage.hourText.getText());
+		CountDownTimerPage.minuteCountDownLabel.setText(CountDownTimerPage.minuteText.getText());
+		CountDownTimerPage.secondsCountDownLabel.setText(CountDownTimerPage.secondsText.getText());
+
+		Activator.setCountDownTime(endTime.getMillis() - startTime.getMillis());
+	}
+	
+	class CountDownTimerTask extends TimerTask {
+
+		@Override
+		public void run() {
+			Duration duration = new Duration(Activator.getCountDownTime());
+			if (duration.getMillis() <= 0) {
+				countDownTimer.cancel();
+				return;
+			}
+			
+			duration = duration.minus(1000);
+			Activator.setCountDownTime(duration.getMillis());
+
+			final Period periodLeft = duration.toPeriod();
+			PeriodFormatter formatter = new PeriodFormatterBuilder().minimumPrintedDigits(2).printZeroAlways().appendHours()
+					.appendSeparator(":").minimumPrintedDigits(2).printZeroAlways().appendMinutes().appendSeparator(":")
+					.minimumPrintedDigits(2).printZeroAlways().appendSeconds().toFormatter();
+			String formattedTime = periodLeft.toString(formatter);
+			Runnable updateUi = () -> {
+				int hours = periodLeft.getHours();
+				int mins = periodLeft.getMinutes();
+				int seconds = periodLeft.getSeconds();
+				NumberFormat numberFormat = new DecimalFormat("00");
+				CountDownTimerPage.hourCountDownLabel.setText(numberFormat.format(hours));
+				CountDownTimerPage.minuteCountDownLabel.setText(numberFormat.format(mins));
+				CountDownTimerPage.secondsCountDownLabel.setText(numberFormat.format(seconds));
+			};
+
+			Display.getDefault().asyncExec(updateUi);
+			
+			Job countDownJob = new CountDownJob("countdown", formattedTime);
+			countDownJob.schedule();
+		}
 		
 	}
 	
-	 private void setInitialTimer() {
-	      String inthours = CountDownTimerPage.hourText.getText();
-	      String intMins = CountDownTimerPage.minuteText.getText();
-	      String intSeconds = CountDownTimerPage.secondsText.getText();
-	      
-	      DateTime startTime = DateTime.now();
-	      
-	      DateTime endTime = startTime.plusHours(Integer.parseInt(inthours)).plusMinutes(Integer.parseInt(intMins)).plus(Integer.parseInt(intSeconds));
-	      
-	      CountDownTimerPage.hourCountDownLabel.setText(CountDownTimerPage.hourText.getText());
-	      CountDownTimerPage.minuteCountDownLabel.setText(CountDownTimerPage.minuteText.getText());
-	      CountDownTimerPage.secondsCountDownLabel.setText(CountDownTimerPage.secondsText.getText());
-	      
-	      Activator.setCountDownTime(endTime.getMillis() - startTime.getMillis());
-	   }
 }
--- bundles/us.nineworlds.xstreamer/src/us/nineworlds/xstreamer/jobs/CountDownJob.java
+++ bundles/us.nineworlds.xstreamer/src/us/nineworlds/xstreamer/jobs/CountDownJob.java
@@ -2,8 +2,6 @@ package us.nineworlds.xstreamer.jobs;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 
 import org.apache.commons.io.FileUtils;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -11,22 +9,18 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.swt.widgets.Display;
-import org.joda.time.Duration;
-import org.joda.time.Period;
-import org.joda.time.format.PeriodFormatter;
-import org.joda.time.format.PeriodFormatterBuilder;
 
 import us.nineworlds.xstreamer.Activator;
-import us.nineworlds.xstreamer.forms.CountDownTimerPage;
 import us.nineworlds.xstreamer.preferences.PreferenceConstants;
 
 public class CountDownJob extends Job {
 	
 	private boolean running = true;
+	private String formattedTime;
 
-	public CountDownJob(String name) {
+	public CountDownJob(String name, String formattedTime) {
 		super(name);
+		this.formattedTime = formattedTime;
 	}
 	
 	@Override
@@ -45,32 +39,6 @@ public class CountDownJob extends Job {
 
 	@Override
 	protected IStatus run(IProgressMonitor monitor) {
-		long timeLeft = Activator.getCountDownTime();
-		if (timeLeft <= 0 || running == false) {
-			running = false;
-			return Status.CANCEL_STATUS;
-		}
-
-		Duration duration = new Duration(Activator.getCountDownTime());
-		duration = duration.minus(1000);
-		Activator.setCountDownTime(duration.getMillis());
-
-		final Period periodLeft = duration.toPeriod();
-		PeriodFormatter formatter = new PeriodFormatterBuilder().minimumPrintedDigits(2).printZeroAlways().appendHours()
-				.appendSeparator(":").minimumPrintedDigits(2).printZeroAlways().appendMinutes().appendSeparator(":")
-				.minimumPrintedDigits(2).printZeroAlways().appendSeconds().toFormatter();
-		String formattedTime = periodLeft.toString(formatter);
-		Runnable updateUi = () -> {
-			int hours = periodLeft.getHours();
-			int mins = periodLeft.getMinutes();
-			int seconds = periodLeft.getSeconds();
-			NumberFormat numberFormat = new DecimalFormat("00");
-			CountDownTimerPage.hourCountDownLabel.setText(numberFormat.format(hours));
-			CountDownTimerPage.minuteCountDownLabel.setText(numberFormat.format(mins));
-			CountDownTimerPage.secondsCountDownLabel.setText(numberFormat.format(seconds));
-		};
-
-		Display.getDefault().asyncExec(updateUi);
 		
 		IPreferenceStore preferenceStore = Activator.getDefault().getPreferenceStore();
 		String timerFilePath = preferenceStore.getString(PreferenceConstants.TIMER_FILE_PATH);
@@ -84,8 +52,6 @@ public class CountDownJob extends Job {
 			e.printStackTrace();
 		}
 		
-		schedule(1000);
-
 		return Status.OK_STATUS;
 	}
 
