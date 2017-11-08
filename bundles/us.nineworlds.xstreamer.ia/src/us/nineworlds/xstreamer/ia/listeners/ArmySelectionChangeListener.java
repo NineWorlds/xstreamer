80064e6c352e08839a2ab64e5d10c18c764ce2ca
David Carver <d_a_carver@yahoo.com>
2017-11-08 09:22:37 -0500

    Issue #62 Save Images when loaded to disk

--- bundles/us.nineworlds.xstreamer.ia/src/us/nineworlds/xstreamer/ia/listeners/ArmySelectionChangeListener.java
+++ bundles/us.nineworlds.xstreamer.ia/src/us/nineworlds/xstreamer/ia/listeners/ArmySelectionChangeListener.java
@@ -1,27 +1,39 @@
 package us.nineworlds.xstreamer.ia.listeners;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.net.URL;
 
+import org.apache.commons.io.IOUtils;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 import uky.article.imageviewer.views.ImageView;
 import us.nineworlds.iadata.command.CommandCard;
 import us.nineworlds.iadata.deployment.Deployment;
+import us.nineworlds.xstreamer.Logger;
 import us.nineworlds.xstreamer.ia.core.Activator;
 import us.nineworlds.xstreamer.ia.forms.AbstractPlayerFormPage;
 import us.nineworlds.xstreamer.ia.lookup.CommandCardLookup;
 import us.nineworlds.xstreamer.ia.lookup.DeploymentsLookup;
 import us.nineworlds.xstreamer.ia.model.CommandCardTreeNode;
 import us.nineworlds.xstreamer.ia.model.DeploymentTreeNode;
+import us.nineworlds.xstreamer.ia.preferences.PreferenceConstants;
 
 
 public class ArmySelectionChangeListener implements ISelectionChangedListener {
 	
+	private static final String XSTREAMER_COMMAND_CARD_IMAGE_NAME = "xstreamer_command_card.png";
+	private static final String XSTREAMER_DEPLOYMENT_CARD_IMAGE_NAME = "xstreamer_deployment_card.png";
 	AbstractPlayerFormPage page;
 	
 	public ArmySelectionChangeListener(AbstractPlayerFormPage page) {
@@ -46,6 +58,7 @@ public class ArmySelectionChangeListener implements ISelectionChangedListener {
 				
 				IViewPart view = findImageViewer();
 				loadImage(foundDeployment.getImageurl(), view);
+				saveImage(XSTREAMER_DEPLOYMENT_CARD_IMAGE_NAME, (ImageView) view);
 			}
 			
 			if (structureSelection.getFirstElement() instanceof CommandCardTreeNode) {
@@ -58,7 +71,8 @@ public class ArmySelectionChangeListener implements ISelectionChangedListener {
 				}
 				
 				IViewPart view = findImageViewer();
-				loadImage(commandCard.getImageurl(), view);				
+				loadImage(commandCard.getImageurl(), view);
+				saveImage(XSTREAMER_COMMAND_CARD_IMAGE_NAME, (ImageView) view);
 			}
 			
 		}
@@ -79,6 +93,26 @@ public class ArmySelectionChangeListener implements ISelectionChangedListener {
 				ex.printStackTrace();
 			}
 		}
+	}
+	
+	private void saveImage(String filename, ImageView imageView) {
+		IPreferenceStore store = us.nineworlds.xstreamer.ia.Activator.getDefault().getPreferenceStore();
+		String outputDirectory = store.getString(PreferenceConstants.TEMPLATE_XSTREAMER_IA_OUTPUT_DIRECTORY);
+		if (outputDirectory == null || imageView == null) {
+			return;
+		}
+
+		ImageLoader imageLoader = new ImageLoader();
+		imageLoader.data = new ImageData[] { imageView.imageCanvas.getImageData() };		
+		FileOutputStream outputStream = null;
+		try {
+			outputStream = new FileOutputStream(new File(outputDirectory, filename));
+			imageLoader.save(outputStream, SWT.IMAGE_PNG);
+		} catch (FileNotFoundException e) {
+			Logger.error("Unable to save image to file: " + filename, e);
+		} finally {
+			IOUtils.closeQuietly(outputStream);
+		}				
 	}
 
 }
--- bundles/us.nineworlds.xstreamer/src/us/nineworlds/xstreamer/forms/listeners/SquadSelectionChangeListener.java
+++ bundles/us.nineworlds.xstreamer/src/us/nineworlds/xstreamer/forms/listeners/SquadSelectionChangeListener.java
@@ -1,16 +1,25 @@
 package us.nineworlds.xstreamer.forms.listeners;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 
+import org.apache.commons.io.IOUtils;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
@@ -18,18 +27,21 @@ import org.osgi.framework.Bundle;
 
 import com.github.xws.Pilot;
 import com.github.xws.Upgrade;
-import com.github.xws.Upgrades;
 
 import uky.article.imageviewer.views.ImageView;
+import us.nineworlds.xstreamer.Logger;
 import us.nineworlds.xstreamer.forms.AbstractPlayerFormPage;
 import us.nineworlds.xstreamer.model.PilotTreeNode;
-import us.nineworlds.xstreamer.model.UpgradeTreeNode;
 import us.nineworlds.xstreamer.model.UpgradeTypeTreeNode;
 import us.nineworlds.xstreamer.model.lookup.PilotLookup;
 import us.nineworlds.xstreamer.model.lookup.UpgradeLookup;
 
 public class SquadSelectionChangeListener implements ISelectionChangedListener {
 	
+	private static final String XWING_DATA_BUNDLE_ID = "us.nineworlds.xstreamer.data";
+	private static final String IMAGEVIEW_BUNDLE_ID = "us.nineworlds.xstreamer.imageview";
+	private static final String XSTREAMER_XWING_PILOT_IMAGE_NAME = "xstreamer_xwing_pilot.png";
+	private static final String XSTREAMER_XWING_UPGRADE_CARD_IMAGE_NAME = "xstreamer_xwing_upgrade_card.png";
 	AbstractPlayerFormPage page;
 	
 	public SquadSelectionChangeListener(AbstractPlayerFormPage page) {
@@ -42,8 +54,8 @@ public class SquadSelectionChangeListener implements ISelectionChangedListener {
 		if (selection instanceof IStructuredSelection) {
 			IStructuredSelection structureSelection = (IStructuredSelection) selection;
 			IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-			IViewPart view = activePage.findView("us.nineworlds.xstreamer.imageview");
-			Bundle dataBundle = Platform.getBundle("us.nineworlds.xstreamer.data");
+			IViewPart view = activePage.findView(IMAGEVIEW_BUNDLE_ID);
+			Bundle dataBundle = Platform.getBundle(XWING_DATA_BUNDLE_ID);
 			
 			if (structureSelection.getFirstElement() instanceof PilotTreeNode) {
 				PilotTreeNode pilot = (PilotTreeNode) structureSelection.getFirstElement();
@@ -58,7 +70,6 @@ public class SquadSelectionChangeListener implements ISelectionChangedListener {
 					loadUpgradeImage(view, dataBundle, (Upgrade)upgradeNode.getValue());
 				} catch (ClassCastException ex) {
 				}
-				
 			}
 		}
 	}
@@ -69,10 +80,11 @@ public class SquadSelectionChangeListener implements ISelectionChangedListener {
 			return;
 		}
 		ImageView imageView = (ImageView) view;					
-		IPath pilotImage = new Path("images/" + upgrade.getImagePath());
-		URL pilotImageUrl = FileLocator.find(dataBundle, pilotImage, null);
+		IPath upgradeImage = new Path("images/" + upgrade.getImagePath());
+		URL upgradeImageUrl = FileLocator.find(dataBundle, upgradeImage, null);
 		
-		loadImage(imageView, pilotImageUrl);
+		loadImage(imageView, upgradeImageUrl);
+		saveImage(XSTREAMER_XWING_UPGRADE_CARD_IMAGE_NAME, imageView);
 	}
 
 
@@ -83,6 +95,7 @@ public class SquadSelectionChangeListener implements ISelectionChangedListener {
 		URL pilotImageUrl = FileLocator.find(dataBundle, pilotImage, null);
 		
 		loadImage(imageView, pilotImageUrl);
+		saveImage(XSTREAMER_XWING_PILOT_IMAGE_NAME, imageView);
 	}
 
 	private void loadImage(ImageView imageView, URL pilotImageUrl) {
@@ -92,5 +105,23 @@ public class SquadSelectionChangeListener implements ISelectionChangedListener {
 			ex.printStackTrace();
 		}
 	}
-
+	
+	private void saveImage(String filename, ImageView imageView) {
+		IPreferenceStore store = us.nineworlds.xstreamer.Activator.getDefault().getPreferenceStore();
+		String outputDirectory = store.getString(us.nineworlds.xstreamer.preferences.PreferenceConstants.TEMPLATE_XSTREAMER_XWING_OUTPUT_DIRECTORY);
+		if (outputDirectory == null || imageView == null) {
+			return;
+		}
+		ImageLoader imageLoader = new ImageLoader();
+		imageLoader.data = new ImageData[] { imageView.imageCanvas.getImageData() };		
+		FileOutputStream outputStream = null;
+		try {
+			outputStream = new FileOutputStream(new File(outputDirectory, filename));
+			imageLoader.save(outputStream, SWT.IMAGE_PNG);
+		} catch (IOException e) {
+			Logger.error("Unable to save image to file: " + filename, e);
+		} finally {
+			IOUtils.closeQuietly(outputStream);
+		}
+	}
 }
