eaeb0bf04063171406a1719cc774fe9c0f045d92
David Carver <d_a_carver@yahoo.com>
2017-11-16 15:22:36 -0500

    Fix NPE when image can not be found.

--- bundles/us.nineworlds.xstreamer/src/us/nineworlds/xstreamer/forms/listeners/LoadOtherCardsImageSelectionListener.java
+++ bundles/us.nineworlds.xstreamer/src/us/nineworlds/xstreamer/forms/listeners/LoadOtherCardsImageSelectionListener.java
@@ -16,11 +16,13 @@ import org.eclipse.ui.IViewPart;
 import org.osgi.framework.Bundle;
 
 import uky.article.imageviewer.views.ImageView;
+import us.nineworlds.xstreamer.model.conditions.Conditions;
 import us.nineworlds.xstreamer.model.damagedeck.DamageDeck;
 
 public class LoadOtherCardsImageSelectionListener extends AbstractImageSelectionListener implements ISelectionChangedListener {
 
 	private static final String XSTREAMER_DAMAGEDECK_IMAGENAME = "xstreamer_damage_card.png";
+	private static final String XSTREAMER_CONDITIONS_IMAGENAME = "xstreamer_conditions_card.png";
 
 	@Override
 	public void selectionChanged(SelectionChangedEvent event) {
@@ -39,6 +41,15 @@ public class LoadOtherCardsImageSelectionListener extends AbstractImageSelection
 			}			
 			loadAndSaveImage(damageDeck, view);
 		}
+		
+		if (structuredSeleciton.getFirstElement() instanceof Conditions) {
+			Conditions conditions = (Conditions) structuredSeleciton.getFirstElement();
+			IViewPart view = findImageViewer();
+			if (view == null) {
+				return;
+			}			
+			loadAndSaveImage(conditions, view);			
+		}
 				
 	}
 
@@ -48,9 +59,26 @@ public class LoadOtherCardsImageSelectionListener extends AbstractImageSelection
 		IPath mapImagePath = new Path("images/" + damageDeck.getImagePath());
 		URL mapImageUrl = FileLocator.find(bundle, mapImagePath, null);
 		try {
-			loadImage(mapImageUrl.openStream(), imageView);
-			saveImage(XSTREAMER_DAMAGEDECK_IMAGENAME, imageView, SWT.IMAGE_PNG);
+			if (mapImageUrl != null) {
+				loadImage(mapImageUrl.openStream(), imageView);
+				saveImage(XSTREAMER_DAMAGEDECK_IMAGENAME, imageView, SWT.IMAGE_PNG);
+			}
 		} catch (IOException e) {
 		}
 	}
+	
+	private void loadAndSaveImage(Conditions conditions, IViewPart view) {
+		ImageView imageView = (ImageView) view;
+		Bundle bundle = Platform.getBundle(XWING_DATA_BUNDLE_ID);
+		IPath mapImagePath = new Path("images/" + conditions.getImagePath());
+		URL mapImageUrl = FileLocator.find(bundle, mapImagePath, null);
+		try {
+			if (mapImageUrl != null) {
+				loadImage(mapImageUrl.openStream(), imageView);
+				saveImage(XSTREAMER_CONDITIONS_IMAGENAME, imageView, SWT.IMAGE_PNG);				
+			}
+		} catch (IOException e) {
+		}
+	}
+
 }
